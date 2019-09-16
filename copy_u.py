936e8791f676d4f3d036b0d0eb1a93e1efee07d7
heizhuer <30657213+Heizhuer@users.noreply.github.com>
2019-09-16 16:36:09 +0800

    Add files via upload

---
+++ copy_u.py
@@ -0,0 +1,17 @@
+from time import sleep
+import os, shutil
+usb_path = "I:\\"
+des_path = r'F:\copy_usb'
+
+
+# 复制I盘下的所有文件(des_path必须不存在)
+while True:
+    try:
+        content = os.listdir(usb_path)
+        print(content)
+        shutil.copytree(usb_path, des_path, False)
+        break
+    except Exception:
+        sleep(3)
+
+from collections import Counter
