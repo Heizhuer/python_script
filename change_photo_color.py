3e56effa4fc939e997386c3ef90569bd8fa91a5f
heizhuer <30657213+Heizhuer@users.noreply.github.com>
2019-09-17 19:10:38 +0800

    Add files via upload

---
+++ change_photo_color.py
@@ -0,0 +1,17 @@
+from removebg import RemoveBg
+from PIL import Image
+
+rmbg = RemoveBg('sy4J7ujaEBLNWWLHYyubbuZx', 'error_log')
+# 在原照片路径下生成png图片
+rmbg.remove_background_from_img_file(r'white.jpg')
+
+
+# 更改证件照底色
+im = Image.open('white.jpg_no_bg.png')
+x, y = im.size
+
+p = Image.new('RGBA', im.size, (255, 0, 0))
+p.paste(im, (0, 0, x, y), im)
+p.save('red.png')
+
+# 网址: https://www.remove.bg
